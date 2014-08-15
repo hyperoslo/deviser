61a460bab62188db330451061ae850700e7acb03
Tim Kurvers <tim@moonsphere.net>
2014-08-15 18:57:28 +0200

    Initial Deviser implementation

---
+++ deviser.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'readline'
+
+require ::File.expand_path('../config/environment',  __FILE__)
+
+puts 'Provide Deviser with an authenticatable resource to impersonate.'
+puts 'Use Ruby (e.g. User.first) in the following prompt:'
+
+loop do
+  begin
+    input = Readline.readline('> ', true)
+    $resource = eval(input)
+    unless $resource.respond_to? :active_for_authentication?
+      raise 'That does not seem like an authenticatable resource :('
+    end
+    break
+  rescue SyntaxError, StandardError => e
+    puts "#{e.class.name}: #{e.message}"
+  end
+end
+
+require 'application_controller'
+
+class ApplicationController
+  prepend_around_filter do
+    sign_in $resource, bypass: true
+  end
+end
+
+session = ActionDispatch::Integration::Session.new(Rails.application)
+session.get('/')
+
+puts
+puts 'Session key:'
+
+key = Rails.configuration.session_options[:key]
+puts session.cookies[key]
