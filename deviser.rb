df50eaa951a7e15b7a9ad5dfecb0d59d5df553c9
Tim Kurvers <tim@moonsphere.net>
2015-09-07 15:35:41 +0200

    Allow devising multiple times

--- deviser.rb
+++ deviser.rb
@@ -4,6 +4,14 @@ require 'readline'
 
 require ::File.expand_path('../config/environment',  __FILE__)
 
+require 'application_controller'
+
+class ApplicationController
+  prepend_around_filter do
+    sign_in $resource, bypass: true
+  end
+end
+
 class String
   def gray
     "\e[90m#{self}\e[0m"
@@ -12,6 +20,16 @@ class String
   def red
     "\e[91m#{self}\e[0m"
   end
+end
+
+def key
+  Rails.configuration.session_options[:key]
+end
+
+def cookie
+  session = ActionDispatch::Integration::Session.new(Rails.application)
+  session.get('/')
+  session.cookies[key]
 end
 
 puts 'Provide Deviser with an authenticatable resource to impersonate.'
@@ -33,27 +51,14 @@ loop do
     unless $resource.respond_to? :active_for_authentication?
       raise 'That does not seem like an authenticatable resource :('.red
     end
-    break
+
+    puts
+    puts "For cookie '#{key}' use the following session key:"
+    puts
+
+    puts cookie
+    puts
   rescue SyntaxError, StandardError => e
     puts "#{e.class.name}: #{e.message}".red
   end
 end
-
-require 'application_controller'
-
-class ApplicationController
-  prepend_around_filter do
-    sign_in $resource, bypass: true
-  end
-end
-
-session = ActionDispatch::Integration::Session.new(Rails.application)
-session.get('/')
-
-key = Rails.configuration.session_options[:key]
-
-puts
-puts "For cookie '#{key}' use the following session key:"
-puts
-
-puts session.cookies[key]
